f0d3fdc38b8e432ac980c473e4e9537169849436
Kamil Shersheyev <kamil.shersheyev@gmail.com>
2016-07-24 17:58:40 +0200

    add gen to server

--- gen.py
+++ gen.py
@@ -5,13 +5,18 @@ import music_prepare.dataset
 import music_prepare.predict_pitches
 import music_prepare.mxml_utils
 
-m = MusicNetwork('normalized_syllables_rhythm_notes.json-seqlen-100.hdf5')
-m.load()
 
-durations, pitches, syllables = m.sample("Happy birthday to you, happy birthday to you, happy birthday dear [Name Here], happy birthday to you.")
+def sample_musicxml(input_string):
+    m = MusicNetwork('normalized_syllables_rhythm_notes.json-seqlen-100.hdf5')
+    m.load()
 
-mxml = music_prepare.mxml_utils.create_mxml(syllables, [''] * len(durations),
-                                            durations, pitches)
+    durations, pitches, syllables = m.sample(input_string)
 
-with open('out.xml', 'w') as f:
-    f.write(mxml)
+    return music_prepare.mxml_utils.create_mxml(syllables, [''] * len(durations),
+                                                durations, pitches)
+
+
+if __name__ == '__main__':
+    mxml = sample_musicxml("Happy birthday to you, happy birthday to you, happy birthday dear [Name Here], happy birthday to you.")
+    with open('out.xml', 'w') as f:
+        f.write(mxml)
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, make_response
 app = Flask(__name__)
+from gen import sample_musicxml
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("form_submit.html")
     if request.method == 'POST':
-        return request.form['source']
+        mxml = sample_musicxml(request.form['source'])
+        response = make_response(mxml)
+        response.headers["Content-Disposition"] = "attachment; filename=out.xml"
+        return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12345, debug=True)
